f0776750731913a590eb9839b88be4c2ccec7883
Paper99 <1398872296@qq.com>
2023-12-01 19:36:09 +0800

    1.1 add onMainGroupMessage

--- src/main/java/cn/paper_card/bilibili_bind/api/BilibiliBindApi.java
+++ src/main/java/cn/paper_card/bilibili_bind/api/BilibiliBindApi.java
@@ -1,6 +1,7 @@
 package cn.paper_card.bilibili_bind.api;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
@@ -12,4 +13,6 @@ public interface BilibiliBindApi {
     @NotNull BindCodeService getBindCodeService();
 
     @NotNull PreLoginResponse handlePreLogin(@NotNull UUID uuid, @NotNull String name);
+
+    @Nullable String onMainGroupMessage(@NotNull String message, long senderQq);
 }
